e92500264a3a8e7863f2391463110903b962836c
김수빈 <57705512+MOBUMIN@users.noreply.github.com>
2023-11-20 22:58:21 +0900

    subin: 105, 106

---
+++ LeetCode/00105/105-subin.ts
@@ -0,0 +1,15 @@
+function buildTree(preorder: number[], inorder: number[]): TreeNode | null {
+	const make = (preorder: number[], inorder: number[]): TreeNode | null => {
+        if (!preorder.length || !inorder.length) {
+            return null;
+        }
+        const rootNode = new TreeNode(preorder.shift());
+        const inorderRootIndex = inorder.indexOf(rootNode.val);
+        rootNode.left = make(preorder, inorder.slice(0, inorderRootIndex));
+        rootNode.right = make(preorder, inorder.slice(inorderRootIndex + 1));
+
+        return rootNode;
+    }
+
+    return make(preorder, inorder)
+};
