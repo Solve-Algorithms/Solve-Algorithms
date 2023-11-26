9c4c4414a4538b3aca92a6fe9773ed1284afc260
Hyun Ku Kim <presentnine@naver.com>
2023-11-26 23:10:59 +0900

    Create 108-hyunku.java

---
+++ LeetCode/00108/108-hyunku.java
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return buildSubTree(nums, 0, nums.length - 1);
+    }
+
+    TreeNode buildSubTree(int[] nums, int start, int end) {//해당하는 구간에 대한 서브트리 생성 함수
+        if (start > end) {
+            return null;
+        }
+
+        int now = (start + end) / 2;//남은 구간의 가운데를 현재 서브 트리의 부모 노드로 지정
+        TreeNode node = new TreeNode(nums[now]);//부모 노드 생성
+
+        node.left = buildSubTree(nums, start, now - 1);//부모 노드를 제외한 왼쪽 구간에 대한 하위 서브 트리 생성
+        node.right = buildSubTree(nums, now + 1, end);//부모 노드를 제외한 오른쪽 구간에 대한 하위 서브 트리 생성
+
+        return node;//노드 반환
+    }
+}
